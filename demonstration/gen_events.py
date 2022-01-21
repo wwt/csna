07a944dc0d1c721938fedaf10b30339ce9947747
Joel W. King <joel.king@wwt.com>
2022-01-21 21:55:18 +0000

    added files in demonstration directory

---
+++ demonstration/gen_events.py
@@ -0,0 +1,116 @@
+#!/opt/phantom/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#      Copyright (c) 2021 - 2022  World Wide Technology
+#      All rights reserved.
+#
+#      author: Joel W. King @joelwking - World Wide Technology
+#
+#      references:
+#          https://kc.mcafee.com/resources/sites/MCAFEE/content/live/CORP_KNOWLEDGEBASE/78000/KB78712/en_US/CEF_White_Paper_20100722.pdf
+import time
+from datetime import datetime
+from datetime import timedelta
+import os
+import yaml
+#
+#  Download from https://raw.githubusercontent.com/joelwking/Phantom-Cyber/master/REST_ingest/PhantomIngest.py
+#
+import PhantomIngest as ingest
+
+# export PH_AUTH_TOKEN=2adnymvMJMredactedHOM+xBGUNs1wEk=
+# export PH_SERVER=54.237.22.123
+
+p = ingest.PhantomIngest(os.getenv('PH_SERVER'), os.getenv('PH_AUTH_TOKEN'))
+
+def get_fileobj(filepath=None):
+    """ Get a file object, handling exceptions and returning False if the file cannot be opened
+    """
+    try:
+        fileobject = open(filepath, 'r')
+    except (IOError, OSError, TypeError) as e:
+        return False
+
+    return fileobject
+
+def add_event(document, custom=False):
+    """
+    """
+
+    try:
+        document['container']
+    except KeyError:
+        print("missing required field 'container'")
+        return False
+
+    container = {}
+
+    for key in ('name', 'description', 'label'):
+        container[key] = document['container'].get(key), 'NONE')
+
+    try:
+        container_id = p.add_container(**container)
+    except AssertionError as e:
+        print("Any HTTP return code other than OK %s" % e)
+        return False
+    except Exception as e:
+        print("Typically the phantom host did not respond, a connection error %s" % e)
+        return False
+    
+    _artifact = {}
+
+    for artifact in document['container'].get('artifacts'):
+        for key in ('name', 'source_data_identifier'):
+            _artifact[key] = artifact.get(key, 'NONE')
+        cef = artifact.get(cef, dict())
+        if custom:
+            cef = add_defaults_for_custom_fields(cef)
+        meta_data = artifact.get(meta_data, dict())
+        try:
+            artifact_id = p.add_artifact(container_id, cef, meta_data, **_artifact)
+            print(f'Added container:{container_id}, artifact:{artifact_id}')
+        except (AssertionError, Exception) as e:
+            print(f'Failure adding artifact: {e}')
+        
+        _artifact = {}
+
+    return
+
+def add_defaults_for_custom_fields(cef):
+    """ 
+    """
+
+    if cef['startTime'] == 'default':
+        cef['startTime'] = time.time()  # milliseconds since epoch
+
+    if cef['deviceCustomDate1'] == 'default':
+        current_time = datetime.utcnow()
+        default_start_time = (current_time - timedelta(minutes=time_span)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        start_time = datetime.strptime(default_start_time, '%Y-%m-%dT%H:%M:%SZ')
+        cef['deviceCustomDate1'] = start_time
+  
+    return cef
+
+
+def main():
+
+    parser = argparse.ArgumentParser(prog='gen_events', description='Create events in Splunk> SOAR')
+    parser.add_argument('-c', '--custom', dest='custom', help='update custom fields if not set', action='store_true'required=False)
+    parser.add_argument('-f', '--file', dest='document', help='a YAML formatted input file', default=None, required=True)
+    args = parser.parse_args()
+
+    stream = get_fileobj(args.document)
+    if stream:
+        documents = yaml.safe_load_all(stream)
+    else:
+        print(f'...nothing to do!')
+        sys.exit(1)
+
+    for document in documents:
+        add_event(document, custom=args.custom)
+
+    stream.close()
+
+if __name__ == '__main__':
+    main()
+
